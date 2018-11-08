6f010e8af4b49bdda8ce62669907e404c85d7e03
Greg <gregmercer99@gmail.com>
2018-11-07 20:04:13 -0800

    Identifies and prints out conflicts

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -24,7 +24,27 @@ void printSchedule(priority_queue<Course> pq) {
 }
 
 void printConflicts(priority_queue<Course> pq) {
+    vector<Course> courses{};
+    while(!pq.empty()) {
+        courses.push_back(std::move(const_cast<Course&>(pq.top())));
+        pq.pop();
+    }
 
+    for (unsigned int i = 0; i < courses.size(); i++) {
+        for(unsigned int j = i + 1; j < courses.size(); j++) {
+            Course x = courses.at(i);
+            Course y = courses.at(j);
+
+            if (x.day == y.day) {
+                if ((x.start_time >= y.start_time && x.start_time <= y.finish_time)
+                    || (x.finish_time >= y.start_time && x.finish_time <= y.finish_time)
+                    || (x.start_time <= y.start_time && x.finish_time >= y.finish_time)) {
+                    cout << "CONFLICT: " << endl;
+                    cout << x << y << endl;
+                }
+            }
+        }
+    }
 }
 
 int main () {
@@ -49,6 +69,7 @@ int main () {
         courses.push(Course(title, dow, start, finish));
     }
 
+    printConflicts(courses);
     printSchedule(courses);
 
     return 0;
